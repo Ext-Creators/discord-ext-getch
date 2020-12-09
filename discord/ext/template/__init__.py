cba79fad62c5a884b1d5ff9f70ff063450080659
Riley Shaw <contact@shiney.dev>
2020-12-09 23:31:08 +0000

    update package init

--- discord/ext/template/__init__.py
+++ discord/ext/template/__init__.py
@@ -1,5 +1,5 @@
 """
-    Copyright 2020 Ext-Creators
+    Copyright (c) 2020 Ext-Creators
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -14,10 +14,9 @@
     limitations under the License.
 """
 
-from collections import namedtuple
+import collections
 
 
-__version__ = "0.1.0a"
-
-_VersionInfo = namedtuple("_VersionInfo", "major minor micro releaselevel serial")
+_VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro releaselevel serial")
 version_info = _VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha", serial=0)
+version = "0.1.0a"
